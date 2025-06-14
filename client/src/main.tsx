c650a349e89ec7a9d62e92242a02245733507603
creafters <belbaghazaoui@gmail.com>
2025-06-14 18:22:38 +0100

    add analytics for vercel

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from '@vercel/analytics/react';
 import App from "./App";
 import "./index.css";
 
@@ -8,4 +9,9 @@ fontAwesome.rel = 'stylesheet';
 fontAwesome.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
 document.head.appendChild(fontAwesome);
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
